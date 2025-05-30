48e6087812f75cf73ffa4a00551ce9cb46267185
DWN11311 <mohamedkhatab2002@gmail.com>
2025-05-30 23:53:57 +0300

    fix: re-add proxy in vercel config

--- src/redux/backendUrl.js
+++ src/redux/backendUrl.js
@@ -1,5 +1,5 @@
-// const backendUrl = "/api";
+const backendUrl = "/api";
 // const backendUrl = "http://localhost:8001";
-const backendUrl = "https://facelog-nest.vercel.app/";
+// const backendUrl = "https://facelog-nest.vercel.app/";
 
 export default backendUrl;
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    // proxy: {
-    //   "/api": {
-    //     // target: "https://facelog-nest.vercel.app",
-    //     target: "http://localhost:8001",
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ""),
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "https://facelog-nest.vercel.app",
+        // target: "http://localhost:8001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
     // https: {
     //   key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
     //   cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
