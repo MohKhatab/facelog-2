780d79a1af9a02d3b966e2669bf901b8dd69691b
DWN11311 <mohamedkhatab2002@gmail.com>
2025-05-31 00:04:07 +0300

    remove proxy server completely

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://facelog-nest.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-
-        headers: {
-          "Access-Control-Allow-Origin": "https://facelog-2.vercel.app",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Credentials": "true",
-        },
-      },
-    },
-  },
   plugins: [tailwindcss(), react()],
 });
